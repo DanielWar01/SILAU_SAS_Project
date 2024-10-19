a038e458b24d87cb4d07a6a4be60ee02f09d4ab7
DanielWar01 <danielalejoguerra@gmail.com>
2024-10-18 21:17:39 -0500

    Home terminado

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { CardProductComponent } from '../../shared/card-product/card-product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, CardProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export default class HomeComponent {
-
+  
 }
