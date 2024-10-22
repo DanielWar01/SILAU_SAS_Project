31c57eb89eafdbdbfbb630cd22979c1fb8fd9216
DanielWar01 <danielalejoguerra@gmail.com>
2024-10-21 19:29:39 -0500

    Modulos de inicio y empresa terminados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { SliderComponent } from "./shared/slider/slider.component";
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,8 @@ import { SliderComponent } from "./shared/slider/slider.component";
   imports: [
     RouterOutlet,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-yellow': '#ee8317',
+        'brand-gray': '#2e2d2b',
+        'brand-red': '#e31c21',
+        'yellow-intense': '#ec7c12',
+      }
+    },
   },
   plugins: [],
 }
